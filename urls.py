ff466fc708f5423d9bc44a36c922cf6591c97340
dhrubang <76925591+dhrubang@users.noreply.github.com>
2025-07-09 05:43:04 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
-
-urlpatterns = [
-    path('', lambda request: redirect('login'), name='home'),  # Redirect root to login
-    path('', include('core.urls')),  # Include core URLs first
-    path('admin/', admin.site.urls),  # Admin URLs after core
-]
